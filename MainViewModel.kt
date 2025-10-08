584f5cc589852e5f21dc4cc8ab655d660adc223e
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-10-08 20:25:45 -0300

    Update MainViewModel.kt
    esse view não está pronto, mas um dia vai estar

--- MainViewModel.kt
+++ MainViewModel.kt
@@ -2,4 +2,17 @@ class MainAcitivity: AppcompatAtivity(){
   private lateint var binding : ActivityMainBindig;
   private viewModel: MainViewModel by viewModel();
   override fun onCreated (savadInstanceState: bundle?){
-    super.onCreate
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    viewModel.usuario.observe(this){
+      usuario?.let{
+        binding.tvNome.text = "nome ${it.nome}"
+        binding.tvIdade.text = "idade ${it.idade}"
+      }
+    }
+    viewModel.carregarUsuario()
+    
+  }
+}
