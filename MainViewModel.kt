1ba703ff6b7912baf6d87439b396be22c9ee534b
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-10-08 20:08:56 -0300

    Create MainViewModel.kt
    ainda não terminei esse main, mas estou tentando o maximo possivel

---
+++ MainViewModel.kt
@@ -0,0 +1,5 @@
+class MainAcitivity: AppcompatAtivity(){
+  private lateint var binding : ActivityMainBindig;
+  private viewModel: MainViewModel by viewModel();
+  override fun onCreated (savadInstanceState: bundle?){
+    super.onCreate
