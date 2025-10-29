939b2289b8780b27ae7b557175a97e3d98c2d9ba
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-10-29 20:19:39 -0300

    Create RetrofitInstance for API service

---
+++ app/src/main/java/com/example/myapplication/RetrofitInstance.kt
@@ -0,0 +1,13 @@
+package com.exemple.myapplication.services
+
+import retroift2.Retrofit
+
+object RetrofitiInstance{
+  private const val BASW_URL = "https://api.github.com/"
+  val api: ApiSerice by lazy {
+    retrofit.Builder()
+          .baseUrl(baseUrl = BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create())
+          .build()
+          .create(ApiServerce::class.java)
+  }
